357c34c829eb14a579f5d931b4c57d4f21500565
naman <namananya5@gmail.com>
2026-08-25 09:17:53 +0530

    Install Microsoft Shopping UET browser tag

--- src/components/integrations/MarketingAnalytics.tsx
+++ src/components/integrations/MarketingAnalytics.tsx
@@ -7,6 +7,7 @@ const GA4_MEASUREMENT_ID =
   process.env.NEXT_PUBLIC_GA4_MEASUREMENT_ID || "G-FSV104S5S3";
 const BING_UET_TAG_ID =
   process.env.NEXT_PUBLIC_BING_UET_TAG_ID || "211072489";
+const MICROSOFT_SHOPPING_UET_TAG_ID = "355060364";
 
 declare global {
   interface Window {
@@ -15,8 +16,12 @@ declare global {
     uetq?: {
       push: (...args: unknown[]) => unknown;
     };
+    shoppingUetq?: {
+      push: (...args: unknown[]) => unknown;
+    };
     __buudyGa4Ready?: boolean;
     __buudyUetReady?: boolean;
+    __buudyShoppingUetReady?: boolean;
   }
 }
 
@@ -71,23 +76,35 @@ function trackGa4PageView() {
 }
 
 function initBingUet() {
-  if (!BING_UET_TAG_ID || window.__buudyUetReady) {
+  if (
+    (!BING_UET_TAG_ID && !MICROSOFT_SHOPPING_UET_TAG_ID) ||
+    (window.__buudyUetReady && window.__buudyShoppingUetReady)
+  ) {
     return;
   }
 
   window.uetq = window.uetq || { push: () => undefined };
+  window.shoppingUetq = window.shoppingUetq || {
+    push: () => undefined,
+  };
 
   const script = document.createElement("script");
   script.id = "buudy-bing-uet-loader";
   script.text = `
-    (function(w,d,t,r,u){
+    (function(w,d,t,r,u,s){
       var f,n,i;
       w[u]=w[u]||[];
+      w[s]=w[s]||[];
       f=function(){
         var o={ti:"${BING_UET_TAG_ID}", enableAutoSpaTracking:true};
         o.q=w[u];
         w[u]=new UET(o);
         w[u].push("pageLoad");
+
+        var shoppingOptions={ti:"${MICROSOFT_SHOPPING_UET_TAG_ID}", enableAutoSpaTracking:true};
+        shoppingOptions.q=w[s];
+        w[s]=new UET(shoppingOptions);
+        w[s].push("pageLoad");
       };
       n=d.createElement(t);
       n.src=r;
@@ -101,10 +118,11 @@ function initBingUet() {
       };
       i=d.getElementsByTagName(t)[0];
       i.parentNode.insertBefore(n,i);
-    })(window,document,"script","https://bat.bing.com/bat.js","uetq");
+    })(window,document,"script","https://bat.bing.com/bat.js","uetq","shoppingUetq");
   `;
   document.head.appendChild(script);
   window.__buudyUetReady = true;
+  window.__buudyShoppingUetReady = true;
 }
 
 export function MarketingAnalytics() {
@@ -127,6 +145,14 @@ export function MarketingAnalytics() {
 
     if (lastTrackedPath.current && window.uetq) {
       window.uetq.push("event", "page_view", {
+        page_path: path,
+        page_location: window.location.href,
+        page_title: document.title,
+      });
+    }
+
+    if (lastTrackedPath.current && window.shoppingUetq) {
+      window.shoppingUetq.push("event", "page_view", {
         page_path: path,
         page_location: window.location.href,
         page_title: document.title,
